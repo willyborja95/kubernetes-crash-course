f48adf5338785764ceee17fe50fabf96688c2b66
willyborja95 <bor_williamalexander@hotmail.com>
2021-01-12 22:08:22 -0500

    Upgrading kompose generated files

--- 05-currency-conversion-microservice-basic/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ 05-currency-conversion-microservice-basic/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")//
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")//
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 //@FeignClient(name = "currency-exchange-service")//Kubernetes Service Name
 public interface CurrencyExchangeServiceProxy {
 	//http://localhost:8000/currency-exchange/from/USD/to/INR
